570075434e0d55ee2d68499f98898ae1084e7145
Bagas Prabaswara <bagasprabaswara77@gmail.com>
2023-04-27 21:06:37 +0700

    feature: management admin page

--- src/route.js
+++ src/route.js
@@ -7,8 +7,11 @@ import SuccessForgotPasswordPage from "./page/success-forgot-password";
 import DashboardPage from "./page/dashboard";
 import ProfilePage from "./page/profile";
 import ChangePasswordPage from "./page/change-password";
+import ManagementAdminPage from "./page/management-admin/management-admin";
 import ProtectedRoutes from "./component/protectedRoute";
 import { AuthContextProvider } from "./component/shared/AuthContext";
+import AddAdminPage from "./page/management-admin/add-admin";
+import EditAdminPage from "./page/management-admin/edit-admin";
 
 const RouterPage = () => {
   return (
@@ -23,6 +26,9 @@ const RouterPage = () => {
               path="/profile/change-password"
               element={<ChangePasswordPage />}
             />
+            <Route exact path="/admin" element={<ManagementAdminPage />} />
+            <Route exact path="/admin/add" element={<AddAdminPage />} />
+            <Route exact path="/admin/edit/:id" element={<EditAdminPage />} />
           </Route>
           <Route path="/user" element={<User />} />
           <Route path="/login" element={<LoginPage />} />
